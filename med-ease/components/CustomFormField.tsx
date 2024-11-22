2598fd8812b88a088e979e5c82813ead4ac65d1f
MugoJames <jamonjoroge816@gmail.com>
2024-11-22 12:01:23 +0300

    Feat:New features

--- med-ease/components/CustomFormField.tsx
+++ med-ease/components/CustomFormField.tsx
@@ -1,7 +1,8 @@
-"use client"
+import { E164Number } from "libphonenumber-js/core";
+import Image from "next/image";
 
 import {
-FormControl,
+  FormControl,
   FormDescription,
   FormField,
   FormItem,
@@ -11,32 +12,85 @@ FormControl,
 import { Input } from "@/components/ui/input"
 import { Control } from "react-hook-form"
 import { FormFieldType } from "./forms/PatientForm"
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+
 interface CustomProps  {
-    control: Control<any>,
-    fieldType : FormFieldType,
-    name: string,
-    label? :string,
-    placeholder? : string,
-    iconSrc? :string,
-    iconAlt? :string,
-    disabled? :boolean,
-    dateFormat? :string,
-    showTimeSelect? :boolean,
-    children? :React.ReactNode,
-    renderSkeleton? :(field: any) => React.ReactNode,
+    control: Control<any>;
+    fieldType : FormFieldType;
+    name: string;
+    label? :string;
+    placeholder? : string;
+    iconSrc? :string;
+    iconAlt? :string;
+    disabled? :boolean;
+    dateFormat? :string;
+    showTimeSelect? :boolean;
+    children? :React.ReactNode;
+    renderSkeleton? :(field: any) => React.ReactNode;
 
 }
 
-const CustomFormField = ({control, fieldType, name, label}: CustomProps) => {
+const RenderField  = ( {field, props}: { field: any; props: CustomProps}) =>{
+  const {fieldType,iconSrc,iconAlt,placeholder} = props;
+  switch (fieldType){
+    case FormFieldType.INPUT:
+      return(
+        <div className="flex rounded-md border border-dark-500 bg-dark-400  ">
+          {iconSrc && (
+            <Image 
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || 'icon'}
+              className='ml-2'
+
+            />
+          )}
+          <FormControl>
+            <input 
+              placeholder={placeholder}
+              {...field}
+              className="shad-input  border-0"
+            />
+          </FormControl>
+        </div>
+      )
+      case FormFieldType.PHONE_INPUT:
+       return (
+        <FormControl>
+          <PhoneInput 
+            defaultCountry="KE"
+            placeholder={placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+
+          />
+      </FormControl>
+       );
+    default:
+      break;
+   
+  }
+
+}
+
+const CustomFormField = (props: CustomProps) => {
+  const {control, fieldType, name, label} = props;
   return (
     <FormField
       control={control}
-      name="name"
+      name={name}
       render={({ field }) => (
       <FormItem className="flex-1">
         {fieldType !== FormFieldType.CHECKBOX && label && (
             <FormLabel>{label}</FormLabel>
         )}
+          <RenderField field={field} props={props}  />
+          <FormMessage className="shad-error" />
 
       </FormItem>
     )}
--- med-ease/components/forms/PatientForm.tsx
+++ med-ease/components/forms/PatientForm.tsx
@@ -3,9 +3,11 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
 import {Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/Validation"
 
 export enum FormFieldType {
     INPUT = "input",
@@ -16,21 +18,22 @@ export enum FormFieldType {
     SELECT="select",
     SKELETON="skeleton",
 }
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+
  
 const  PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof UserFormValidation>) {
     console.log(values)
   }
   return (
@@ -43,7 +46,7 @@ const  PatientForm = () => {
         </section>
 
         <CustomFormField 
-            fieldType = {FormFieldType.INPUT}
+           fieldType = {FormFieldType.INPUT}
            control={form.control}
            name="name"
            label="Full name"
@@ -51,8 +54,25 @@ const  PatientForm = () => {
            iconSrc="/assets/icons/user.svg"
            iconAlt="User"
         />
+        <CustomFormField 
+           fieldType = {FormFieldType.INPUT}
+           control={form.control}
+           name="email"
+           label="Email"
+           placeholder="johndoe@gmail.com"
+           iconSrc="/assets/icons/email.svg"
+           iconAlt="email"
+        />
+          <CustomFormField 
+           fieldType = {FormFieldType.PHONE_INPUT}
+           control={form.control}
+           name="phone"
+           label="phone number"
+           placeholder="+254 112 234 567"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
     
-      <Button type="submit">Submit</Button>
     </form>
   </Form>
   )
